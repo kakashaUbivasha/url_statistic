39094ca7aa6b23fa23686d81626f7927efbb6a75
kakashaUbivasha <makspala777@gmail.com>
2025-07-02 14:55:24 +0500

    Link model

--- database/migrations/2025_07_02_095156_create_links_table.php
+++ database/migrations/2025_07_02_095156_create_links_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('original_url');
+            $table->string('short_code')->unique();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('links');
     }
 };
